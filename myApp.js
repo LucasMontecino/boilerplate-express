d0161ac0d0a9ef7a4a46860745d8abc0327dbc84
Lucas Montecino <lucasmontecino95@gmail.com>
2024-03-29 11:59:41 -0300

    commit

--- myApp.js
+++ myApp.js
@@ -1,39 +1,18 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+app.get("/", (req, res) => {
+  let path = __dirname + "/views/index.html";
 
+  res.sendFile(path);
+});
 
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
+});
 
+app.use("/public", express.static(__dirname + "/public"));
 
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
